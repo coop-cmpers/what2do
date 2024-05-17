f5bea4ca183e7d129445b0ce612cbbabf5ab6b2c
Shreyam982 <166836308+Shreyam982@users.noreply.github.com>
2024-05-17 15:31:06 +1000

    get the page working (#4)

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Paper } from "@mui/material";
+
+const Home = () => {
+  return (
+    <Paper>
+      <div>
+        <h1>"THIS IS NODES"</h1>
+      </div>
+    </Paper>
+  );
+}
+
+export default Home;
