196a1aafe109b9d332605375ceafd60c242376cb
BlackNinja1369 <77535139+BlackNinja1369@users.noreply.github.com>
2024-05-18 12:18:59 +1000

    seperated the HelloWorld function into own file (#8)

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,25 +1,16 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net"
 
 	"github.com/coop-cmpers/what2do-backend/src/endpoints"
 
-	pb "github.com/coop-cmpers/what2do-backend/protos-gen/helloworld/v1"
+	helloWorldpb "github.com/coop-cmpers/what2do-backend/protos-gen/helloworld/v1"
 	what2dopb "github.com/coop-cmpers/what2do-backend/protos-gen/what2do/v1"
 
 	"google.golang.org/grpc"
 )
-
-type server struct {
-	pb.UnimplementedHelloWorldServiceServer
-}
-
-func (s *server) HelloWorld(ctx context.Context, req *pb.HelloWorldRequest) (*pb.HelloWorldResponse, error) {
-	return &pb.HelloWorldResponse{Message: "Hi, My name is Slim Shady!"}, nil
-}
 
 func main() {
 	port := "12345"
@@ -30,7 +21,7 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	pb.RegisterHelloWorldServiceServer(s, &server{})
+	helloWorldpb.RegisterHelloWorldServiceServer(s, &endpoints.HelloWorldServer{})
 	what2dopb.RegisterWhat2DoServiceServer(s, &endpoints.What2doServer{})
 
 	log.Printf("gRPC server listening at %v", listen.Addr())
