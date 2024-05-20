ae1a44160ca86164ca178f4c451aee519d3784c6
BlackNinja1369 <77535139+BlackNinja1369@users.noreply.github.com>
2024-05-20 23:50:18 +1000

    adding in the place api backend (#14)

--- backend/src/endpoints/search-recommendations.go
+++ backend/src/endpoints/search-recommendations.go
@@ -4,16 +4,14 @@ import (
 	"context"
 
 	pb "github.com/coop-cmpers/what2do-backend/protos-gen/what2do/v1"
+	"github.com/coop-cmpers/what2do-backend/src/clients"
 )
 
+
 func (s *What2doService) SearchRecommendations(ctx context.Context, req *pb.SearchRecommendationsRequest) (*pb.SearchRecommendationsResponse, error) {
-	return &pb.SearchRecommendationsResponse{
-		Recommendations: []*pb.Recommendation{
-			{
-				Rank:        1,
-				Address:     "AA",
-				Description: "BB",
-			},
-		},
-	}, nil
+	recommendation, err := clients.FetchPlacesFromPlacesAPI(ctx, req.SearchParam, req.Location, req.EventTime)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.SearchRecommendationsResponse{Recommendations: recommendation}, err
 }
--- frontend/src/pages/dev.tsx
+++ frontend/src/pages/dev.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { searchRecommendations } from "../clients/recommendations";
+import { Recommendation } from "../protos-gen/what2do/v1/recommendations_pb";
+import { Timestamp } from "@bufbuild/protobuf";
 
 const DevPage = () => {
+  const [recommendations, setRecommendations] = useState<Recommendation[]>([]);
+  const [searchParam, setSearchParam] = useState<string>("");
+  const [location, setLocation] = useState<string>("");
+  const handleSearchButton = () => {
+    searchRecommendations({searchParam: searchParam, location: location, eventTime: Timestamp.now()}).then((resp) => setRecommendations(resp.recommendations));
+  }
+
   return (
     <>
-      <p>testtt</p>
+      <input type="text" onChange={(e) => setSearchParam(e.target.value)}></input>
+      <input type="text" onChange={(e) => setLocation(e.target.value)}></input>
+      <button onClick={() => handleSearchButton()}>Search</button>
+      {recommendations.map((recommendation) => (
+        <div>
+        <h3>{recommendation.name}</h3>
+        <p>{recommendation.address}</p>
+        <p>{recommendation.PriceLevel}</p>
+        <p>{recommendation.Open?.toJsonString()}</p>
+        </div>
+      ))}
     </>
   );
 }
